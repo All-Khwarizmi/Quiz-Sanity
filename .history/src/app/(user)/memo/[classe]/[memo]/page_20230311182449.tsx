63cad880b07364bab27949855b8ce784dfee3f6b
Jason Suarez <swarecito@gmail.com>
2023-03-11 19:28:42 +0100

    modified UI on large screen and changed quiz validation behavior

---
+++ .history/src/app/(user)/memo/[classe]/[memo]/page_20230311182449.tsx
@@ -0,0 +1,41 @@
+import Quiz from '@/components/Quiz';
+import { cache } from 'react';
+import { memoFetcher, QuizType } from '../../../../../../lib/memoFetcher';
+import { client } from '../../../../../../lib/sanity.client';
+import { SINGLEMEMO } from '../../../../../../queries/queries';
+type Params = {
+  params: { classe: string; memo: string };
+};
+type QuizProps = {
+  quiz: QuizType;
+};
+const clientFetch = cache(client.fetch.bind(client));
+const page = async ({ params }: Params) => {
+  const newParams = {
+    ...params,
+    memo: params.memo.replace('%20', ' '),
+  };
+  console.log("" params)
+  let quiz: QuizType = await memoFetcher(newParams.memo, newParams.classe);
+
+  console.log('Quiz classe', quiz.classe);
+  // console.log('Questions', data[0].question);
+  return (
+    <>
+      <main className='min-h-screen w-screen'>
+        <Quiz
+          questions={quiz.questions}
+          started={false}
+          finished={false}
+          points={0}
+          malus={0}
+          length={0}
+          memo={quiz.memo}
+          classe={quiz.classe}
+        />
+      </main>
+    </>
+  );
+};
+
+export default page;
